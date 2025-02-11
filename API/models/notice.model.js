d6c4e834e7f00bcce3c59fa5534e0e9082489513
Milad Coder <miladcoder1390@gmail.com>
2025-02-11 17:44:16 +0330

    Add notice schema

---
+++ API/models/notice.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const noticeSchema = new mongoose.Schema({
+    school: { type: mongoose.Schema.ObjectId, ref: 'School' },
+    title:{type: String, requird: true},
+    message: {type:String, required: true},
+    audience:{type:String, enum:['student', 'teacher'], required: true},    
+
+
+
+
+    createdAt: { type: Date, default: new Date() }
+})
+
+
+module.exports = mongoose.model('Notice', noticeSchema)
