1520ad958e8af94706eeaa84b45754db436c1aa1
Milad Coder <miladcoder1390@gmail.com>
2025-02-14 01:04:39 +0330

    Use authorization for school routers

--- API/routers/school.router.js
+++ API/routers/school.router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const authMiddleware = require("../auth/auth.js")
 const { registerSchool, getAllSchools, loginSchool, updateSchool, getSchoolOwnData } = require('../controllers/school.contoller');
 
 const router = express.Router();
@@ -9,8 +10,8 @@ const router = express.Router();
 router.post('/register', registerSchool);   // * School register  router
 router.get('/all', getAllSchools);  // * Get all schools router
 router.get('/login', loginSchool);  // *  School login router
-router.patch('update', updateSchool);   // * School update router 
-router.get('/fetch-single', getSchoolOwnData);  // * School fetch single data router
+router.patch('update', authMiddleware(['SCHOOL']), updateSchool);   // * School update router 
+router.get('/fetch-single', authMiddleware(['SCHOOL']), getSchoolOwnData);  // * School fetch single data router
 
 
 module.exports = router;
