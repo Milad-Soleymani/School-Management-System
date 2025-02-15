bb8bc4ffd93763a66311e77cf4feec4d63d65ac3
Milad Coder <miladcoder1390@gmail.com>
2025-02-15 20:14:14 +0330

    Add school components routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,14 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import School from './school/school';
 import Attendance from './school/components/attendance/attendance';
+import Class from './school/components/class/Class';
+import Dashboard from './school/components/dashboard/dashboard';
+import Examinations from './school/components/examinations/examinations';
+import Notice from './school/components/notice/notice';
+import Schedule from './school/components/schedule/schedule';
+import Students from './school/components/students/Students';
+import Subjects from './school/components/subjects/Subjects';
+import Teachers from './school/components/teachers/Teachers';
 
 function App() {
 
@@ -13,6 +21,14 @@ function App() {
           
             <Route path='school' element={<School />}>
             <Route path='attendance' element={<Attendance />} />
+            <Route path='class' element={<Class />} />
+            <Route path='dashboard' element={<Dashboard />} />
+            <Route path='examinations' element={<Examinations />} />
+            <Route path='notice' element={<Notice />} />
+            <Route path='schedule' element={<Schedule />} />
+            <Route path='students' element={<Students />} />
+            <Route path='subjects' element={<Subjects />} />
+            <Route path='teachers' element={<Teachers />} />
 
           </Route>
           {/* STUDENT */}
