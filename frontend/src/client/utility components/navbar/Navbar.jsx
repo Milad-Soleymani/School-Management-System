6728aa3549229b45cffe37e8b1c917eac14b1a49
Milad Coder <miladcoder1390@gmail.com>
2025-02-16 20:42:18 +0330

    Delete User Menu(not required)

--- frontend/src/client/utility components/navbar/Navbar.jsx
+++ frontend/src/client/utility components/navbar/Navbar.jsx
@@ -137,28 +137,7 @@ function Navbar() {
                                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
                             </IconButton>
                         </Tooltip>
-                        <Menu
-                            sx={{ mt: '45px' }}
-                            id="menu-appbar"
-                            anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={Boolean(anchorElUser)}
-                            onClose={handleCloseUserMenu}
-                        >
-                            {settings.map((setting) => (
-                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                    <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
+
                     </Box>
                 </Toolbar>
             </Container>
