94542d096f822cbd13f19945a55d3c4b77b25207
Milad Coder <miladcoder1390@gmail.com>
2025-02-16 19:50:36 +0330

    Add client file(homepage main page)

---
+++ frontend/src/client/Client.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+export default function Client() {
+
+
+
+    return (
+        <>
+            <h1>Client Main component</h1>
+            <Outlet />
+        </>
+    )
+}
