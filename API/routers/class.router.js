3104d3bd897f07f66aa246c0c469ee0ea7068c2b
Milad Coder <miladcoder1390@gmail.com>
2025-05-09 17:59:46 +0330

    Add class route(s)

---
+++ API/routers/class.router.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const authMiddleware = require("../auth/auth.js");
+const {
+    createClass,
+    getAllClasses,
+    updateClassWithId,
+    deleteClassWithId
+} = require('../controllers/class.controller.js');
+
+
+const router = express.Router();
+
+//! Routers | روت‌ها
+
+// 📜 Create a new class | ایجاد یک کلاس جدید
+router.post('/create', authMiddleware(['SCHOOL']),createClass);
+
+// 📜 Get all registered classes | دریافت لیست تمامی کلاس های ثبت‌شده
+router.get('/all', authMiddleware(['SCHOOL']), getAllClasses);
+
+// 📜 Update class data | به‌روزرسانی اطلاعات کلاس
+router.patch('/update/:id', authMiddleware(['SCHOOL']), updateClassWithId);
+
+// 📜 Delete class | حذف کلاس 
+router.get('/delete/:id', authMiddleware(['SCHOOL']), deleteClassWithId);
+
+// 📦 Export router | صادر کردن روت‌ها
+module.exports = router;
