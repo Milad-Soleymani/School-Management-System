ca19e31797d06552a213906d8a01aaa317f38fef
Milad Coder <miladcoder1390@gmail.com>
2025-05-09 11:59:14 +0330

    Recive ownschooldata

--- frontend/src/school/components/dashboard/Dashboard.jsx
+++ frontend/src/school/components/dashboard/Dashboard.jsx
@@ -1,5 +1,27 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { baseApi } from "../../../enviorment"
+
 export default function Dashboard() {
 
+    const [school, setSchool] = useState(null)
+
+    const fetchSchool = () => {
+
+        axios.get(`${baseApi}/school/fetch-single`).then((res) => {
+            console.log(res)
+            setSchool(res.data.school)
+        }).catch(e => {
+            console.log(e)
+        })
+    } 
+    useEffect(()=> {
+        
+        fetchSchool();
+
+
+
+    },[])
 
     return (
         <>
